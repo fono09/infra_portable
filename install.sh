f7a74341029376ef2d32a13bf1679aa3c6b6ee9c
fono09 <fono@fono.jp>
2016-11-08 11:56:21 +0900

    Use and Mount vpn_server.config"

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ ovs-vsctl add-port $VSWITCH_NAME $IFACE
 
 ## SoftEtherVPN
 docker run -d --cap-add NET_ADMIN --privileged --name $SE_CT_NAME --net=none \
-        -e PSK=$PSK -e USERS="${USERS}" -e SPW=$SPW -e HPW=$HPW \
+	-v ./vpn_server.config:/opt/vpn_server.config \
         siomiz/softethervpn
 
 ovs-docker add-port $VSWITCH_NAME veth0 $SE_CT_NAME --ipaddress=$SE_CT_ADDR --gateway=$CT_GATEWAY
